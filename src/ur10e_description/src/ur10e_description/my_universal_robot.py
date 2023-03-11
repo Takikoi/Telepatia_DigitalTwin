d2fbc5ea0925f7eb702a7b3196a5224fffe1e589
Khoi M. Le <khoile.workuser@gmail.com>
2023-03-11 16:14:32 +0700

    can now read from /wrench topic

--- src/ur10e_description/src/ur10e_description/my_universal_robot.py
+++ src/ur10e_description/src/ur10e_description/my_universal_robot.py
@@ -8,21 +8,22 @@ from rospy_message_converter import message_converter
 
 class MyUniversalRobot:
     
-    joint_state = ''
+    joint_state = {}
+    wrench_state = {}
 
     def __init__(self) -> None:
         pass
 
     def set_joint_state(self, msgs):
-        # self.joint_state = json_message_converter.convert_ros_message_to_json(msgs)
         self.joint_state = message_converter.convert_ros_message_to_dictionary(msgs)
 
-    def update_stream(self, stream_file_path):
-            # checks if file exists
-        if os.path.isfile(stream_file_path) and os.access(stream_file_path, os.R_OK):
-            print ("File exists and is readable")
-        else:
-            print ("Either file is missing or is not readable, creating file...")
-        
+    def update_joint_stream(self, stream_file_path):
         with io.open(stream_file_path, 'w') as f:
             f.write(json.dumps(self.joint_state, indent=2))
+
+    def set_wrench_state(self, msgs):
+        self.wrench_state = message_converter.convert_ros_message_to_dictionary(msgs)
+
+    def update_wrench_stream(self, stream_file_path):
+        with io.open(stream_file_path, 'w') as f:
+            f.write(json.dumps(self.wrench_state, indent=2))
--- src/ur10e_msgs_processor/scripts/ur10e_joint_state_reader.py
+++ src/ur10e_msgs_processor/scripts/ur10e_joint_state_reader.py
@@ -1,20 +1,26 @@
 import rospy
 from control_msgs.msg import JointTrajectoryControllerState
+from geometry_msgs.msg import Wrench
 from rospy_message_converter import json_message_converter
 from ur10e_description import my_universal_robot as UR
 
-PATH_TO_STREAM = 'src/ur10e_msgs_processor/stream/stream.json'
+PATH_TO_JOINT_STREAM = 'src/ur10e_msgs_processor/stream/stream_joint.json'
+PATH_TO_WRENCH_STREAM = 'src/ur10e_msgs_processor/stream/stream_wrench.json'
 
 my_ur10e = UR.MyUniversalRobot()
 
-def callback(data):
-    rospy.loginfo(type(data))
+def callback_pos(data):
     my_ur10e.set_joint_state(data)
-    my_ur10e.update_stream(PATH_TO_STREAM)
+    my_ur10e.update_joint_stream(PATH_TO_JOINT_STREAM)
+
+def callback_wrench(data):
+    my_ur10e.set_wrench_state(data)
+    my_ur10e.update_wrench_stream(PATH_TO_WRENCH_STREAM)
 
 def subscriber():
     rospy.init_node("ur10e_joint_state_reader" , anonymous=True)
-    rospy.Subscriber("/eff_joint_traj_controller/state", JointTrajectoryControllerState, callback)
+    rospy.Subscriber("/scaled_pos_joint_traj_controller/state", JointTrajectoryControllerState, callback_pos)
+    rospy.Subscriber("/wrench", Wrench, callback_wrench)
     rospy.spin()
 
 if __name__ == '__main__':
